40a277982441d2e7e29eb1f600d70dcde671ad59
corto <corto.garnier1@gmail.com>
2025-03-01 13:14:44 +0100

    Update hello.py to include additional entries in the DataFrame

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 # Create a DataFrame
 data = {
-    'name': ['John Doe', 'Jane Smith'],
-    'description': ['A sample description 1', 'A sample description 2'],
-    'title': ['Title 1', 'Title 2']
+    'name': ['John Doe', 'Jane Smith', 'vador'],
+    'description': ['A sample description 1', 'A sample description 2', 'A sample description 3'],
+    'title': ['Title 1', 'Title 2', 'Title 3']
 }
 
 df = pd.DataFrame(data)
